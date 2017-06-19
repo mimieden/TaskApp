3fe8aa6953f9655a9615b83c00b53ad9e22b3b3b
mimieden <mimieden525@gmail.com>
2017-06-20 02:13:04 +0900

    0.11 temp

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,54 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by mimieden on 2017/06/19.
+//  Copyright © 2017年 mimieden. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+//==================================================
+// グローバル変数/定数
+//==================================================
+//--Outlet(RestrationIDもセット!)---------------------
+    //タイトル(テキストフィールド)のOutlet
+    @IBOutlet weak var O_TitleTextField: UITextField!
+    //内容(TextView)のOutlet
+    @IBOutlet weak var O_ContentsTextView: UITextView!
+    //Date PickerのOutlet
+    @IBOutlet weak var O_DatePicker: UIDatePicker!
+    
+//--------------------------------------------------
+    var task: Task!   // 追加する (V_TaskにするとVC.swiftでエラー)
+    
+//==================================================
+//  関数(ライフサイクル)
+//==================================================
+//--View読み込み後------------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//--------------------------------------------------
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
